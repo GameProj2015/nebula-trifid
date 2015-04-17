5b7681b6634d1700705b40e6cc075a606ecc1b7e
mario <mario@localhost>
2015-04-17 14:06:21 +0200

    added fps camera

--- code/application/corefeature/properties/fpscameraproperty.h
+++ code/application/corefeature/properties/fpscameraproperty.h
@@ -19,35 +19,62 @@
     (C) 2013-2014 Individual contributors, see AUTHORS file
 */
 
-#include "basegamefeature/managers/factorymanager.h"
 #include "game/property.h"
-#include "basegamefeature/basegameprotocol.h"
 #include "graphicsfeature/graphicsattr/graphicsattributes.h"
+#include "graphicsfeatureproperties.h"
 
 namespace FPSCameraFeature
 {
-class FPSCameraProperty : public Game::Property
-{
-	__DeclareClass(FPSCameraProperty);
-	__SetupExternalAttributes();
-public:
-    /// constructor
-    FPSCameraProperty();
-    /// destructor
-    virtual ~FPSCameraProperty();
-    /// called from Entity::DeactivateProperties()
-    virtual void OnDeactivate();
-    /// setup accepted messages
-    virtual void SetupAcceptedMessages();
-    /// setup callbacks for this property
-    virtual void SetupCallbacks();    
-    /// return true if currently has camera focus
-    virtual bool HasFocus() const;    
-    /// handle a single message
-    virtual void HandleMessage(const Ptr<Messaging::Message>& msg);
-protected:
-};
-__RegisterClass(FPSCameraProperty);
+	class FPSCameraProperty : public GraphicsFeature::CameraProperty
+	{
+		__DeclareClass(FPSCameraProperty);
+		__SetupExternalAttributes();
+	public:
+		/// constructor
+		FPSCameraProperty();
+		/// destructor
+		virtual ~FPSCameraProperty();
+		/// called from Entity::DeactivateProperties()
+		virtual void OnDeactivate();
+		/// setup accepted messages
+		virtual void SetupAcceptedMessages();
+		/// setup callbacks for this property
+		virtual void SetupCallbacks();
+		/// called from within Entity::OnStart() after OnLoad when the complete world exist
+		virtual void OnStart();
+		/// called when camera focus is obtained
+		virtual void OnObtainCameraFocus();
+		/// called when camera focus is lost
+		virtual void OnLoseCameraFocus();
+		/// called before rendering happens
+		virtual void OnRender();
+		/// return true if currently has camera focus
+		virtual bool HasFocus() const;
+		/// handle a single message
+		virtual void HandleMessage(const Ptr<Messaging::Message>& msg);
+		/// called when input focus is gained
+		virtual void OnObtainInputFocus();
+		/// called when input focus is lost
+		virtual void OnLoseInputFocus();
+		/// return true if currently has input focus
+		/// called on begin of frame
+		virtual void OnBeginFrame();
+	protected:
 
-}; // namespace GraphicsFeature
+		///Get joint position by index
+		Math::vector GetJointPos(IndexT index);
+
+		/// update audio listener position
+		void UpdateAudioListenerPosition() const;
+		Ptr<Graphics::ModelEntity> modelEntity;
+		float rotx, roty, fov, closeplane, farplane, sensitivity;
+		Util::StringAtom head;
+		Util::String hip;
+		IndexT headIndex;
+		IndexT hipIndex;
+		Ptr<Game::Entity> ent;
+	};
+	__RegisterClass(FPSCameraProperty);
+
+}; // namespace FPSCameraFeature
 //------------------------------------------------------------------------------
